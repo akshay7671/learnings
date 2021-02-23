f5c528de9c71d4ccb0e49b7225cda0f5d6a2af93
Akshay7671 <akshayjadhav7671@gmail.com>
2021-02-23 11:57:33 +0530

    junit5 in progress. intital setup completed

---
+++ src/test/java/com/samples/test/junit_mockito/ContactManagerTest.java
@@ -0,0 +1,13 @@
+package com.samples.test.junit_mockito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactManagerTest {
+
+    @Test
+    public void shouldCreateContact() {
+    }
+
+}
